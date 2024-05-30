b9b51719e0c460c77ef35857a3f3ee63b8d8989b
LuthMC <134492162+LuthMC@users.noreply.github.com>
2024-05-31 00:22:52 +0700

    Add Main.php

---
+++ src/Luthfi/ReportBug/Main.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ReportBug;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use Luthfi\ReportBug\commands\ReportBugCommand;
+use Luthfi\ReportBug\commands\ViewBugsCommand;
+
+class Main extends PluginBase {
+
+    private static $instance;
+    private $reportedBugs = [];
+
+    public static function getInstance(): self {
+        return self::$instance;
+    }
+
+    public function onEnable(): void {
+        self::$instance = $this;
+
+        $this->getServer()->getCommandMap()->register("reportbug", new ReportBugCommand($this));
+        $this->getServer()->getCommandMap()->register("rb", new ViewBugsCommand($this));
+
+        $this->getLogger()->info(TextFormat::GREEN . "ReportBug plugin enabled.");
+    }
+
+    public function addReportedBug(string $playerName, string $bugDescription): void {
+        $this->reportedBugs[] = [
+            "player" => $playerName,
+            "description" => $bugDescription,
+            "timestamp" => time()
+        ];
+    }
+
+    public function getReportedBugs(): array {
+        return $this->reportedBugs;
+    }
+}
